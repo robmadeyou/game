e4f6996b91cd895246ff9d6a3e83f8388c633f78
robmadeyou <robmadeyou@gmail.com>
2016-04-26 14:17:27 +0100

    Adding some text to keep us entertained

--- src/com/game/GameView.java
+++ src/com/game/GameView.java
@@ -1,7 +1,10 @@
 package com.game;
 
+import com.abereth.draw.shapes.Square;
 import com.abereth.game.Game;
 import com.abereth.game.View;
+import com.abereth.gui.text.Text;
+
 
 public class GameView extends View
 {
@@ -11,8 +14,16 @@ public class GameView extends View
     }
 
     @Override
+    public void initialize ()
+    {
+        Text t = new Text ( "", 0, 0, 0, 0 );
+        t.set ( "Awesome game 2k16 with a slice of toast and some beans" );
+        add ( t );
+
+    }
+
+    @Override
     public void update(int delta)
     {
-
     }
 }
