7cd9a78b74487699086a0a815be9856cd67be7b1
robmadeyou <robmadeyou@gmail.com>
2016-04-26 10:35:01 +0100

    Works

--- src/com/game/Main.java
+++ src/com/game/Main.java
@@ -1,11 +1,13 @@
 package com.game;
 
+import com.abereth.G;
 import com.abereth.game.Game;
 
 public class Main
 {
     public static void main (String[] args)
     {
+        G.ARP = "./lib/Abereth/res/";
         Game g = new Game( 500, 500 );
         g.addView( new GameView( g ) );
         g.start();
