e80aac1b9cfc65a1cd7fbe5f13b1afbd03fe22dc
robmadeyou <robmadeyou@gmail.com>
2016-04-27 15:28:28 +0100

    Mostly changes to how Abereth works ( for the better.. I promise )

--- src/com/game/GameView.java
+++ src/com/game/GameView.java
@@ -1,8 +1,10 @@
 package com.game;
 
+import com.abereth.draw.shapes.Square;
 import com.abereth.game.Game;
 import com.abereth.game.View;
 import com.abereth.gui.text.Text;
+import com.abereth.input.Mouse;
 
 
 public class GameView extends View
@@ -24,5 +26,9 @@ public class GameView extends View
     @Override
     public void update(int delta)
     {
+        if( Mouse.isLeftMouseClicked () )
+        {
+            add( new Square ( this.getMouseX (), this.getMouseY (), 20, 20 ) );
+        }
     }
 }
